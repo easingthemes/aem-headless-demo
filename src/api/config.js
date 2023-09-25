f0fafedfc932df1f7313922fac8c1cd5ebef9e95
Dragan Filipovic <dragan.filipovic@netcentric.biz>
2023-09-25 21:19:25 +0200

    [aem] remove post from csrf, update package

--- src/api/config.js
+++ src/api/config.js
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-  serviceURL: 'http://localhost:5502',
+  serviceURL: 'http://localhost:4502',
   endpoint: '/content/_cq_graphql/adaptTo/endpoint.json',
   auth: ['admin', 'admin']
 }
